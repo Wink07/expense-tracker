c4aa84efe6653bd3eae905ba2ef43d5907c5547d
Sergio <winksousa0@gmail.com>
2022-08-15 12:25:25 -0300

    first commit

---
+++ src/data/items.ts
@@ -0,0 +1,28 @@
+import { Item } from "../types/Item";
+
+export const items: Item[] = [
+  {
+    date: new Date(2022, 5, 11),
+    category: "food",
+    title: "McDonalds",
+    value: 32.12,
+  },
+  {
+    date: new Date(2022, 5, 11),
+    category: "food",
+    title: "Burguer King",
+    value: 25.12,
+  },
+  {
+    date: new Date(2022, 5, 11),
+    category: "rent",
+    title: "Aluguel Apt",
+    value: 600.12,
+  },
+  {
+    date: new Date(2022, 5, 11),
+    category: "salary",
+    title: "Salario Mes",
+    value: 3332.12,
+  },
+];
